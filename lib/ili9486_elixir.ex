3e28ffd12031afb1fea7b719de0e1c44ed413b5a
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-23 09:03:29 +0000

    init

---
+++ lib/ili9486_elixir.ex
@@ -0,0 +1,18 @@
+defmodule Ili9486Elixir do
+  @moduledoc """
+  Documentation for `Ili9486Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ili9486Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ili9486_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Ili9486ElixirTest do
+  use ExUnit.Case
+  doctest Ili9486Elixir
+
+  test "greets the world" do
+    assert Ili9486Elixir.hello() == :world
+  end
+end
