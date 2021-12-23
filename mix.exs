aeeabafc53e1ce818bde630c2c4ca9f3ffa28c35
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-23 13:05:19 +0000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Ili9486Elixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      source_url: "https://github.com/cocoa-xu/ili9486_elixir"
     ]
   end
 
@@ -21,6 +26,26 @@ defmodule Ili9486Elixir.MixProject do
       {:circuits_gpio, "~> 0.4"},
       {:circuits_spi, "~> 0.1"},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp elixirc_paths(_), do: ~w(lib)
+
+  defp docs() do
+    [
+      groups_for_functions: [
+        API: & &1[:functions] == :exported,
+        Constants: & &1[:functions] == :constants
+      ]
+    ]
+  end
+
+  defp package() do
+    [
+      name: "ili9486_elixir",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/cocoa-xu/ili9486_elixir"}
     ]
   end
 end
